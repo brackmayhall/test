d8a9e1b64fc3aeb6861519eefc50ab083acb423c
Brackston Mayhall <brackmayhall@gmail.com>
2020-09-14 11:27:15 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,44 +1,54 @@
 <template>
-  <div class="Auth">
-    <amplify-authenticator username-alias="email">
-      <amplify-sign-up
-      validationErrors="true"
-        slot="sign-up"
-        username-alias="email"
-        :form-fields.prop="formFields"
-      ></amplify-sign-up>
-      <amplify-sign-in slot="sign-in" username-alias="email"></amplify-sign-in>
-    </amplify-authenticator>
-  </div>
+    <div>
+
+
+<b-card-group class="pt-4 justify-content-md-center">
+    <b-col cols="3" >
+      <b-card bg-variant="light" header="Fund Managers" class="text-center font-weight-bold">
+        <p class="h1 mb-2"><b-icon icon="headset"></b-icon></p>
+        <b-card-text>Fund Managers</b-card-text>
+         <b-button variant="primary" to="fundmanagers">Go To Fund Managers</b-button>
+      </b-card>
+    </b-col>
+      <b-col cols="3">
+      <b-card bg-variant="light" header="Fund Managers" class="text-center font-weight-bold">
+        <p class="h1 mb-2"><b-icon icon="headset"></b-icon></p>
+        <b-card-text>Fund Managers</b-card-text>
+         <b-button variant="primary" to="fundmanagers">Go To Fund Managers</b-button>
+      </b-card>
+    </b-col>
+    <b-col cols="3" >
+      <b-card bg-variant="light" header="Fund Managers" class="text-center font-weight-bold">
+        <p class="h1 mb-2"><b-icon icon="headset"></b-icon></p>
+        <b-card-text>Fund Managers</b-card-text>
+         <b-button variant="primary" to="fundmanagers">Go To Fund Managers</b-button>
+      </b-card>
+    </b-col>
+</b-card-group>
+
+<b-card-group class="pt-4 justify-content-md-center">
+
+      <b-col cols="3">
+      <b-card bg-variant="light" header="Fund Managers" class="text-center font-weight-bold">
+        <p class="h1 mb-2"><b-icon icon="headset"></b-icon></p>
+        <b-card-text>Fund Managers</b-card-text>
+         <b-button variant="primary" to="fundmanagers">Go To Fund Managers</b-button>
+      </b-card>
+    </b-col>
+    <b-col cols="3" >
+      <b-card bg-variant="light" header="Fund Managers" class="text-center font-weight-bold">
+        <p class="h1 mb-2"><b-icon icon="headset"></b-icon></p>
+        <b-card-text>Fund Managers</b-card-text>
+         <b-button variant="primary" to="fundmanagers">Go To Fund Managers</b-button>
+      </b-card>
+    </b-col>
+</b-card-group>
+
+
+
+    </div>
 </template>
 
 <script>
-
-export default {
-  name: 'Auth',
-  data() {
-    return {
-      formFields: [
-        {
-          type: 'email',
-          label: 'Email Address *',
-          placeholder: 'Email',
-          required: true,
-        },
-        {
-          type: 'password',
-          label: 'Password *',
-          placeholder: 'Password',
-          required: true,
-        }
-      ]
-    }
-  }
-}
 </script>
-<style lang="scss">
-.Auth {
-  margin: 50px auto;
-  width: 460px;
-}
-</style>
+
