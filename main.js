ffd7b87d0cd5589c119d855a12595a7fa6a28ccf
Brackston Mayhall <brackmayhall@gmail.com>
2020-09-14 11:36:48 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,53 +1,10 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-
-import { Auth } from 'aws-amplify'
-
-import Home from "../views/Home.vue";
-import FundManager from '../views/FundManager.vue'
-import AuthComponent from '../views/Auth.vue'
-
-
-Vue.use(VueRouter);
-
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home,
-    meta: { requiresAuth: true }
-  },
-  {
-    path: "/login",
-    name: "auth",
-    component: AuthComponent,
-    meta: { requiresAuth: false }
-  },
-  {
-    path: "/fundmanager",
-    name: "fundmanager",
-    component: FundManager,
-    meta: { requiresAuth: true }
-  }
-];
-
-const router = new VueRouter({
-  // mode: "history",
-  base: process.env.BASE_URL,
-  routes
-});
-
-router.beforeResolve((to, from, next) => {
-  if (to.matched.some(record => record.meta.requiresAuth)) {
-    Auth.currentAuthenticatedUser().then(() => {
-      next()
-    }).catch(() => {
-      next({
-        path: '/login'
-      });
-    });
-  }
-  next()
-})
-
-export default router
+    "@aws-amplify/ui-vue": "^0.2.15",
+    "aws-amplify": "^3.0.25",
+    "axios": "^0.20.0",
+    "bootstrap": "^4.5.2",
+    "bootstrap-vue": "^2.16.0",
+    "core-js": "^3.6.5",
+    "lodash": "^4.17.20",
+    "moment": "^2.27.0",
+    "vue": "^2.6.11",
+    "vue-router": "^3.2.0"
