87ead64d7e48367bd9e61fe0e1991da24e157282
Brackston Mayhall <brackmayhall@gmail.com>
2020-09-14 11:25:06 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,44 @@
-@import '../node_modules/bootstrap/scss/bootstrap';
-@import '../node_modules/bootstrap-vue/src/index.scss';
+<template>
+  <div class="Auth">
+    <amplify-authenticator username-alias="email">
+      <amplify-sign-up
+      validationErrors="true"
+        slot="sign-up"
+        username-alias="email"
+        :form-fields.prop="formFields"
+      ></amplify-sign-up>
+      <amplify-sign-in slot="sign-in" username-alias="email"></amplify-sign-in>
+    </amplify-authenticator>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Auth',
+  data() {
+    return {
+      formFields: [
+        {
+          type: 'email',
+          label: 'Email Address *',
+          placeholder: 'Email',
+          required: true,
+        },
+        {
+          type: 'password',
+          label: 'Password *',
+          placeholder: 'Password',
+          required: true,
+        }
+      ]
+    }
+  }
+}
+</script>
+<style lang="scss">
+.Auth {
+  margin: 50px auto;
+  width: 460px;
+}
+</style>
